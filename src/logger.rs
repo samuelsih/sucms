d6d7e513f78440049f74df196c5288b85d01f12e
samuelsih <samuel.205025@mhs.its.ac.id>
2023-03-22 21:43:51 +0700

    simplify logging

--- src/logger.rs
+++ src/logger.rs
@@ -1,16 +1,18 @@
 use console::style;
+use std::sync::atomic::{AtomicU8, Ordering};
 
-pub struct Logger {
-    length: usize,
+static LENGTH: AtomicU8 = AtomicU8::new(0);
+
+fn get_current_log_progress() -> u8 {
+    return LENGTH.load(Ordering::Relaxed);
 }
 
-impl Logger {
-    pub fn new() -> Logger {
-        Logger { length: 0 }
-    }
+fn inc_log_progress() {
+    let num = get_current_log_progress() + 1;
+    LENGTH.store(num, Ordering::Relaxed);
+} 
 
-    pub fn log(&mut self, msg: String) {
-        self.length += 1;
-        println!("{} {}", style(format!("[{}] ", self.length)).bold(), msg);
-    }
+pub fn log(msg: String) {
+    inc_log_progress();
+    println!("{} {}", style(format!("[{}] ", get_current_log_progress())).bold(), msg);
 }
